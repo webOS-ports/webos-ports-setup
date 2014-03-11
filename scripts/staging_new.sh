c0f11215e0f6bc00326bbfc1efbb8291f406dd8b
Martin Jansa <Martin.Jansa@gmail.com>
2014-03-12 00:48:50 +0100

    staging_new: Add tagging of layers when closing the staging
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- scripts/staging_new.sh
+++ scripts/staging_new.sh
@@ -24,17 +24,17 @@ ssh ${REMOTE} mkdir ${STAGING_DIR}/wip
 ssh ${REMOTE} ln -snf ${CURRENT_STAGING} ${STAGING_DIR}/latest
 
 export TAG=webOS-ports/dora/${CURRENT_STAGING};
-[ -e scripts/oebb.sh ] && ( OE_SOURCE_DIR=/OE/shr-core scripts/oebb.sh tag ${TAG} )
-cd webos-ports;
+[ -e scripts/oebb.sh ] && ( OE_SOURCE_DIR=${CURRENT_DIR}/${CURRENT_PROJECT} scripts/oebb.sh tag ${TAG} )
 # only for dirs with possible r-w access
 for i in openembedded-core meta-smartphone meta-webos-ports; do
-  cd $i;
+  cd ${CURRENT_PROJECT}/$i;
   git push origin ${TAG};
-  cd ..;
+  cd -;
 done;
-cd buildhistory;
-git tag -a -m "${TAG}" ${TAG}; git push origin ${TAG};
-cd ..;
+cd ${CURRENT_PROJECT}/buildhistory;
+git tag -a -m "${TAG}" ${TAG};
+git push origin ${TAG};
+cd -;
 # cannot do this, because prservice is running on different host
 # cp ../PRservice/prserv.sqlite3 ~/web/shr-core-staging/${TAG}/prserv.sqlite3.${TAG}
 
