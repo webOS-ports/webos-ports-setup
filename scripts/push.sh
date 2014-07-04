49037146e30fff77fa141291c329df1e452dd269
Martin Jansa <Martin.Jansa@gmail.com>
2014-07-04 11:55:42 +0200

    scripts: update for LuneOS
    DISTRO_BUILD_NAME: use WEBOS_DISTRO_BUILD_CODENAME instead of WEBOS_DISTRO_TOPDIR_BRANCH
    
    * so that correct WEBOS_DISTRO_TOPDIR_BRANCH can be shown in /etc/buildinfo
      in my case it will be e.g. jansa/scarthgap instead of just scarthgap
    
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export DATE=`date "+%Y-%m-%d"`
-for B in kirkstone langdale mickledore nanbield; do
-    git tag -a -f -m "${B}-${DATE}" "${B}-${DATE}" ${B}
-    git push -f shr ${B}-${DATE} ${B} jansa/${B}
+for B in scarthgap styhead nodistro-styhead; do
+#     git tag -a -f -m "${B}-${DATE}" "${B}-${DATE}" ${B}
+#     git push -f shr ${B}-${DATE} ${B} jansa/${B}
+     git push -f origin jansa/${B}
 done
