877497c3dcaac2fda79b780fa80536223c7f4435
Martin Jansa <Martin.Jansa@gmail.com>
2014-07-04 11:55:42 +0200

    scripts: update for LuneOS
    DISTRO_BUILD_NAME: use WEBOS_DISTRO_BUILD_CODENAME instead of WEBOS_DISTRO_TOPDIR_BRANCH
    
    * so that correct WEBOS_DISTRO_TOPDIR_BRANCH can be shown in /etc/buildinfo
      in my case it will be e.g. jansa/scarthgap instead of just scarthgap
    
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-SCRIPTDIR=/OE/lge/layers/scripts
+SCRIPTDIR=/OE/layers/scripts
 
-BASE="origin/master"
+BASE="origin/scarthgap"
 BRANCHES="\
-  ose-master \
-  kirkstone \
-  langdale \
-  mickledore \
-  nanbield \
-"
-${SCRIPTDIR}/rebase-multiple.sh ${BASE} "${BRANCHES}" $1
-
-BASE="kirkstone"
-BRANCHES="\
-  kirkstone \
-  jansa/kirkstone \
-  jansa/langdale \
-  jansa/mickledore \
-  jansa/nanbield \
-"
-${SCRIPTDIR}/rebase-multiple.sh ${BASE} "${BRANCHES}" skip_ru
-
-BASE="jansa/kirkstone"
-BRANCHES="\
-  jansa/kirkstone \
-  jansa/nodistro-kirkstone \
-  jansa/nodistro-langdale \
-  jansa/nodistro-mickledore \
-  jansa/nodistro-nanbield \
+  scarthgap \
+  jansa/scarthgap \
+  jansa/styhead \
+  jansa/nodistro-styhead \
 "
 ${SCRIPTDIR}/rebase-multiple.sh ${BASE} "${BRANCHES}" skip_ru
