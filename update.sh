1a6a71120aacc18e70f3472d4eb701998efe2da7
Martin Jansa <Martin.Jansa@gmail.com>
2016-01-18 20:15:35 +0100

    update.sh: add simple script to update revisions in layers.txt
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pushd `dirname $0` > /dev/null
+SCRIPT_DIR=`pwd -P`
+popd > /dev/null
+
+MSG=${SCRIPT_DIR}/commit-message.txt
+
+echo -e "conf/layers.txt: Use latest revisions" > ${MSG}
+
+cat conf/layers.txt  | grep -v HEAD | while IFS="," read DIR URL BRANCH REV; do
+NREV=`cd ~/$DIR; git log origin/$BRANCH | head -n1 | awk '{ print $2 }'`
+echo "DIR=$DIR URL=$URL BRANCH=$BRANCH REV=$REV NREV=$NREV" >&2
+if [ $REV != ${NREV} ] ; then
+    echo -e "\n$DIR:" >> ${MSG}
+    cd ~/$DIR; git log --oneline ${REV}..${NREV} >> ${MSG}; cd -
+    sed -i "s/$REV/$NREV/g" conf/layers.txt
+fi
+done
+
+git commit -s conf/layers.txt -t ${MSG}
