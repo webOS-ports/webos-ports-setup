f5d2e8f7f8f9458f6ede85f622df0a8fe80c56df
Martin Jansa <Martin.Jansa@gmail.com>
2016-08-08 14:16:12 +0200

    update.sh: show the included range in the header

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ cat conf/layers.txt  | grep -v HEAD | while IFS="," read DIR URL BRANCH REV; do
 NREV=`cd ~/$DIR; git log origin/$BRANCH | head -n1 | awk '{ print $2 }'`
 echo "DIR=$DIR URL=$URL BRANCH=$BRANCH REV=$REV NREV=$NREV" >&2
 if [ $REV != ${NREV} ] ; then
-    echo -e "\n$DIR:" >> ${MSG}
+    echo -e "\n$DIR: ${REV}..${NREV}" >> ${MSG}
     cd ~/$DIR; git log --oneline ${REV}..${NREV} >> ${MSG}; cd -
     sed -i "s/$REV/$NREV/g" conf/layers.txt
 fi
