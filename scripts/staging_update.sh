7585c58a0fd19c41ac8e6a557eba9721585803b0
Martin Jansa <Martin.Jansa@gmail.com>
2017-10-04 08:38:27 +0000

    staging_update.sh: create SOURCE_DIR before update
    * when building in clean workspace tmp-glibc won't exist soon enough
      to contain info file during the update causeing:
      tee: webos-ports/tmp-glibc/deploy/info.052: No such file or directory
      Updating sources
      tee: webos-ports/tmp-glibc/deploy/info.052: No such file or directory
      Wed Oct  4 08:15:16 UTC 2017
      tee: webos-ports/tmp-glibc/deploy/info.052: No such file or directory
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- scripts/staging_update.sh
+++ scripts/staging_update.sh
@@ -2,6 +2,7 @@
 
 . scripts/staging_header.sh
 
+mkdir -p ${SOURCE_DIR}
 echo "Updating sources" | tee -a ${SOURCE_DIR}/info.${CURRENT_STAGING}
 date        | tee -a ${SOURCE_DIR}/info.${CURRENT_STAGING}
 make update | tee -a ${SOURCE_DIR}/info.${CURRENT_STAGING}
