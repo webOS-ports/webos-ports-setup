aa608cb132fa440218d995439904dea29339c826
Martin Jansa <Martin.Jansa@gmail.com>
2020-05-27 16:08:19 +0200

    update.sh: run 'git remote update origin' in case the layers aren't up2date already
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- update.sh
+++ update.sh
@@ -7,15 +7,18 @@ SCRIPT_DIR=`pwd -P`
 popd > /dev/null
 
 MSG=${SCRIPT_DIR}/commit-message.txt
+RU="git remote update origin"
 
 echo -e "conf/layers.txt: Use latest revisions" > ${MSG}
 
 cat conf/layers.txt  | grep -v HEAD | while IFS="," read DIR URL BRANCH REV; do
-NREV=`cd ${LAYERS_DIR}/$DIR; git log origin/$BRANCH | head -n1 | awk '{ print $2 }'`
+NREV=`cd ${LAYERS_DIR}/$DIR; $RU 1>&2; git log origin/$BRANCH | head -n1 | awk '{ print $2 }'`
 echo "DIR=$DIR URL=$URL BRANCH=$BRANCH REV=$REV NREV=$NREV" >&2
 if [ $REV != ${NREV} ] ; then
     echo -e "\n$DIR: ${REV}..${NREV}" >> ${MSG}
-    cd ${LAYERS_DIR}/$DIR; git log --oneline ${REV}..${NREV} >> ${MSG}; cd -
+    cd ${LAYERS_DIR}/$DIR
+    git log --oneline ${REV}..${NREV} >> ${MSG}
+    cd -
     sed -i "s/$REV/$NREV/g" conf/layers.txt
 fi
 done
