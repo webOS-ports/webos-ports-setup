fb9139b19b5b0ed9f5d4d0fda62cdd45188fbae7
Martin Jansa <Martin.Jansa@gmail.com>
2023-07-26 10:15:43 +0200

    scripts: update for scarthgap
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -2,10 +2,10 @@
 
 SCRIPTDIR=/OE/layers/scripts
 
-RELEASE="nanbield"
+RELEASE="master"
 BUILD="luneos"
 
-BUILD_BRANCH="jansa/${RELEASE}"
+BUILD_BRANCH="jansa/scarthgap"
 BRANCH="jansa/${RELEASE}"
 BASE_LAYERS=" \
   meta-webos-ports \
