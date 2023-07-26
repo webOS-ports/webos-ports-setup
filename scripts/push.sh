61c9860de9e3d2a67b6cec8018a1f8d65c0590a5
Martin Jansa <Martin.Jansa@gmail.com>
2023-07-26 10:02:42 +0200

    scripts: update for LuneOS
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export DATE=`date "+%Y-%m-%d"`
-for B in kirkstone langdale mickledore nanbield; do
-    git tag -a -f -m "${B}-${DATE}" "${B}-${DATE}" ${B}
-    git push -f shr ${B}-${DATE} ${B} jansa/${B}
+for B in kirkstone langdale mickledore nanbield nodistro-nanbield scarthgap nodistro-scarthgap; do
+#     git tag -a -f -m "${B}-${DATE}" "${B}-${DATE}" ${B}
+#     git push -f shr ${B}-${DATE} ${B} jansa/${B}
+     git push -f origin jansa/${B}
 done
