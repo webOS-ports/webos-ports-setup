e9221489c8fe39b304509d790071b1ce7c0b05f6
Martin Jansa <martin.jansa@gmail.com>
2026-01-20 17:31:10 +0100

    scripts: use whinlatter
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -2,10 +2,10 @@
 
 SCRIPTDIR=/OE/layers/scripts
 
-RELEASE="master"
+RELEASE="whinlatter"
 BUILD="luneos"
 
-BUILD_BRANCH="jansa/whinlatter"
+BUILD_BRANCH="jansa/${RELEASE}"
 BRANCH="jansa/${RELEASE}"
 BASE_LAYERS=" \
   meta-webos-ports \
