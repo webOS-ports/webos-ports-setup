2dcd16ff5b0cc3e3fe2a0600723007995ee8477c
Martin Jansa <martin.jansa@gmail.com>
2026-09-05 09:41:11 +0200

    scripts: use blacksail
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -2,10 +2,10 @@
 
 SCRIPTDIR=/OE/layers/scripts
 
-RELEASE="wrynose"
+RELEASE="master"
 BUILD="luneos"
 
-BUILD_BRANCH="jansa/${RELEASE}"
+BUILD_BRANCH="jansa/blacksail"
 BRANCH="jansa/${RELEASE}"
 BASE_LAYERS=" \
   meta-webos-ports \
