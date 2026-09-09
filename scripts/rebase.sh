2f8fd2fef101b3c5609ace5d7df569e30eeb0edd
Martin Jansa <martin.jansa@gmail.com>
2026-09-09 09:11:56 +0200

    rebase.sh: use wrynose
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -2,10 +2,10 @@
 
 SCRIPTDIR=/OE/layers/scripts
 
-RELEASE="master"
+RELEASE="wrynose"
 BUILD="nodistro"
 
-BUILD_BRANCH="jansa/nodistro-wrynose"
+BUILD_BRANCH="jansa/nodistro-${RELEASE}"
 BRANCH="jansa/${RELEASE}"
 BASE_LAYERS=" \
   meta-openembedded \
