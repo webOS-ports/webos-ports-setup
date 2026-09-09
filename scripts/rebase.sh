29657848b546371408eb428af327e07ae3c09640
Martin Jansa <martin.jansa@gmail.com>
2026-09-09 09:10:55 +0200

    scripts: use master for blacksail
    Signed-off-by: Martin Jansa <martin.jansa@gmail.com>

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -2,10 +2,10 @@
 
 SCRIPTDIR=/OE/layers/scripts
 
-RELEASE="wrynose"
+RELEASE="master"
 BUILD="nodistro"
 
-BUILD_BRANCH="jansa/nodistro-${RELEASE}"
+BUILD_BRANCH="jansa/nodistro-blacksail"
 BRANCH="jansa/${RELEASE}"
 BASE_LAYERS=" \
   meta-openembedded \
